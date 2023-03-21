6eb42344133277a96f117912eb5aa2c495f025b2
Alden Vallestero <aldenvallestero@Aldens-MacBook-Air.local>
2023-03-21 13:12:05 +0800

    feat: add get endpoint

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: 'Hello World!',
+    })
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Started running on port', PORT));
